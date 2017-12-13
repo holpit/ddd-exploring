87b70194c4b2e7b6bec2b8f65e383881a0ac18bf
Ivano <ivano.modenin@softwareuno.it>
2017-12-13 19:15:51 +0100

    Creata Solution Master

---
+++ ddd-exploring/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddd_exploring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
